181b3e403282341a3a589ae21276526d6f018f2e
Renato <medinadato@gmail.com>
2017-09-21 13:49:04 +1000

    Fixed issue when bring up the virtual machine

--- project.sh
+++ project.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # - - - - - - - - -  - - - - - - - - - -
 # @company      Soul + Wolf
-# @author       Renato Medina <medina@mdnsolutions.com>
+# @author       Renato Medina <renato@soulandwolf.com.au>
+# @updatedAt    2017-07-24
 # - - - - - - - - -  - - - - - - - - - -
 
 # - - - - - - - - -  - - - - - - - - - -
@@ -46,6 +47,54 @@ if [ ! -f docker-compose.yml ]; then
 fi
 
 # - - - - - - - - -  - - - - - - - - - -
+# Containers
+# - - - - - - - - -  - - - - - - - - - -
+echo -e "${GREEN}Stopping all the running containers...${NC}"
+docker stop $(docker ps -q)
+sleep 2
+
+mkdir -p $ROOT_DIR'/www'
+
+for var in "$@"
+do
+    if [ "$var" == "--install" ]; then
+        echo -e "${GREEN}Setting up Magento version configurations${NC}"
+        if [ "$MAGENTO_VERSION" == "m1" ]; then
+            cat $ROOT_DIR/docker/etc/nginx/magento1_sample.conf > $ROOT_DIR/docker/etc/nginx/default.conf
+            cat $ROOT_DIR/docker/php/Dockerfile_php56 > $ROOT_DIR/docker/php/Dockerfile
+        elif [ "$MAGENTO_VERSION" == "m2" ]; then
+            cat $ROOT_DIR/docker/etc/nginx/magento2_sample.conf > $ROOT_DIR/docker/etc/nginx/default.conf
+            cat $ROOT_DIR/docker/php/Dockerfile_php70 > $ROOT_DIR/docker/php/Dockerfile
+        fi
+        sed -i -e "s/local.sample.com/$SERVER_NAME/g" $ROOT_DIR/docker/etc/nginx/default.conf
+
+        # Settings
+        sed -i -e "s/sample-mysql/$CONTAINER_DB/g" $ROOT_DIR/docker-compose.yml
+        sed -i -e "s/sample-php/$CONTAINER_PHP/g" $ROOT_DIR/docker-compose.yml
+        sed -i -e "s/sample-nginx/$CONTAINER_NGNIX/g" $ROOT_DIR/docker-compose.yml
+        sed -i -e "s/sample-redis/$CONTAINER_REDIS/g" $ROOT_DIR/docker-compose.yml
+
+        echo -e "${GREEN}Starting containers...${NC}"
+        docker-compose up -d
+        sleep 2
+
+        echo -e "${GREEN}Running custom install commands...${NC}"
+        source install_commands.sh
+        
+        echo -e "${BLUE}- Note: Add the line below to your '/etc/hosts' file:${NC}"
+        echo -e "${BLUE}127.0.0.1 mysql redis${NC}"
+    fi
+done
+
+echo -e "${GREEN}Starting containers...${NC}"
+docker-compose up -d
+sleep 2
+
+# Shows status
+echo -e "${GREEN}Checking containers...${NC}"
+docker ps
+
+# - - - - - - - - -  - - - - - - - - - -
 # Environment
 # - - - - - - - - -  - - - - - - - - - -
 # Non-GNU/Linux platforms need a VM
@@ -71,51 +120,3 @@ if [ "$(expr substr $(uname -s) 1 5)" != "Linux" ]; then
     #docker-machine regenerate-certs $VM_NAME
     sleep 1
 fi
-
-# - - - - - - - - -  - - - - - - - - - -
-# Containers
-# - - - - - - - - -  - - - - - - - - - -
-echo -e "${GREEN}Stopping all the running containers...${NC}"
-docker stop $(docker ps -q)
-sleep 2
-
-mkdir -p $ROOT_DIR'/www'
-
-for var in "$@"
-do
-    if [ "$var" == "--install" ]; then
-        echo -e "${GREEN}Setting up Magento version configurations${NC}"
-        if [ "$MAGENTO_VERSION" == "m1" ]; then
-            cat $ROOT_DIR/docker/etc/nginx/magento1_sample.conf > $ROOT_DIR/docker/etc/nginx/default.conf
-            cat $ROOT_DIR/docker/php/Dockerfile_php56 > $ROOT_DIR/docker/php/Dockerfile
-        elif [ "$MAGENTO_VERSION" == "m2" ]; then
-            cat $ROOT_DIR/docker/etc/nginx/magento2_sample.conf > $ROOT_DIR/docker/etc/nginx/default.conf
-            cat $ROOT_DIR/docker/php/Dockerfile_php70 > $ROOT_DIR/docker/php/Dockerfile
-        fi
-
-        # Settings
-        sed -i -e "s/sample-mysql/$CONTAINER_DB/g" $ROOT_DIR/docker-compose.yml
-        sed -i -e "s/sample-php/$CONTAINER_PHP/g" $ROOT_DIR/docker-compose.yml
-        sed -i -e "s/sample-nginx/$CONTAINER_NGNIX/g" $ROOT_DIR/docker-compose.yml
-        sed -i -e "s/sample-redis/$CONTAINER_REDIS/g" $ROOT_DIR/docker-compose.yml
-
-        echo -e "${GREEN}Starting containers..."
-        docker-compose up -d
-        sleep 2
-
-        echo -e "${GREEN}Running custom install commands...${NC}"
-        source install_commands.sh
-        
-        echo -e "${BLUE}- Note: Add the line below to your '/etc/hosts' file:${NC}"
-        echo -e "${BLUE}127.0.0.1 mysql redis${NC}"
-    fi
-done
-
-echo -e "${GREEN}Starting containers..."
-docker-compose up -d
-sleep 2
-
-# Shows status
-echo -e "${GREEN}Checking containers...${NC}"
-docker ps
-
